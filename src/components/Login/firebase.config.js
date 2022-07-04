7582e02e5a7300215af6cfd8a02c63464659c176
ZIAN786 <zakirzian41@gmail.com>
2022-07-04 23:35:50 +0600

    Burj-Al-Arab

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCa-surGRKHVLDmOXKwAS7mg9U8bB6OQU",
+  authDomain: "burj-al-arab-f6f8c.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-f6f8c.firebaseio.com",
+  projectId: "burj-al-arab-f6f8c",
+  storageBucket: "burj-al-arab-f6f8c.appspot.com",
+  messagingSenderId: "635722015492",
+  appId: "1:635722015492:web:c9ca7a85733245e302a651",
+};
+export default firebaseConfig;
